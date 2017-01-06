9bc6e72d5b8fbac8b4330f1f551467731914f0ea
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2017-01-06 01:04:12 +0100

    Primera version de la plantilla SQL, añadido en Sample el ejemplo con el que se probaran las plantillas

---
+++ Debugging/PlantillasSQL.sql
@@ -0,0 +1,19 @@
+﻿
+		CREATE TABLE IF NOT EXISTS Alumno
+		(
+	Nombre
+Apellido1
+Apellido2
+		);
+			CREATE TABLE IF NOT EXISTS Asignatura
+		(
+	Nombre
+		);
+			CREATE TABLE IF NOT EXISTS Profesor
+		(
+	Nombre
+Apellido1
+Apellido2
+Email
+		);
+	
